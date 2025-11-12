54a052e34ba43f5074910ff6e7de397306120f34
JAMESDONNELLAN-STUDE <G00434671@ATU.IE>
2025-11-12 14:51:33 +0000

    Updated Feature - DataLoader

--- src/main/java/ie/atu/week5/lab5cicd/DataLoader.java
+++ src/main/java/ie/atu/week5/lab5cicd/DataLoader.java
@@ -1,6 +1,9 @@
 package ie.atu.week5.lab5cicd;
 
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
 @Component
 public class DataLoader implements CommandLineRunner
 {
